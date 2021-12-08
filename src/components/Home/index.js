ca4e020a96a0a8c7b8387bf3a157fb5f4db5c4e7
inno sufiyan <innosufiyan@gmail.com>
2021-12-08 12:57:50 +0500

    8 december_3

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import ImgSlider from '../ImgSlider';
+import Viewers from '../Viewers';
 
 
 function Home() {
     return (
         <Container>
             <ImgSlider />
+            <Viewers />
         </Container>
     )
 }
@@ -18,6 +20,7 @@ const Container = styled.main`
     min-height : calc(100vh - 70px);
     padding : 0 calc(3.5vw + 5px);
     position : relative;
+    overflow-x : hidden;
 
     &:before {
         background : url("/images/home-background.png") center center / cover no-repeat fixed;
